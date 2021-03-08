bd8a08775f2214c8340f8661ffe56939e830c7d0
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-03-08 14:51:24 -0300

    Pesquisando os restaurantes usando o Id nas rotas; configurando as routas children

---
+++ src/app/restaurant-detail/shopping-cort/shopping-cort.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-shopping-cort',
+  templateUrl: './shopping-cort.component.html'
+})
+export class ShoppingCortComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
